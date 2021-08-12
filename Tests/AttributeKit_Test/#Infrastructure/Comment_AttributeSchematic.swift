52601c7073162af29e48cf10af6b1d08d2cc8f98
Gor Gyolchanyan <gor.zone@icloud.com>
2021-08-12 20:06:44 +0300

    Add `Comment_AttributeSchematic` to `AttributeKit_Test` target.

---
+++ Tests/AttributeKit_Test/#Infrastructure/Comment_AttributeSchematic.swift
@@ -0,0 +1,18 @@
+//
+// Introductory information can be found in the `README.md` file located at the root of the repository that contains this file.
+// Licensing information can be found in the `LICENSE` file located at the root of the repository that contains this file.
+//
+
+import AttributeKit
+
+@frozen
+internal enum Comment_AttributeSchematic: AttributeSchematic {
+
+    // MARK: Type: AttributeSchematic
+
+    internal typealias Value = String
+
+    internal static var defaultValue: String {
+        ""
+    }
+}
