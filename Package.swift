3c1a6d15139ace45f4a51a8ee62560e1295aa7ef
Gor Gyolchanyan <gor.gyolchanyan@icloud.com>
2021-08-12 19:40:24 +0300

    Add `AttributeKit` swift package.

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.4
+
+//
+// Introductory information can be found in the `README.md` file located at the root of the repository that contains this file.
+// Licensing information can be found in the `LICENSE` file located at the root of the repository that contains this file.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "AttributeKit",
+    products: [
+        .library(
+            name: "AttributeKit",
+            targets: ["AttributeKit"]
+        )
+    ],
+     targets: [
+        .target(
+            name: "AttributeKit"
+        )
+     ]
+)
